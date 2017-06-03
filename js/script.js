9b1e885ae2e319092c59c17a30c527955a834308
krushiraj <krushiraj123@gmail.com>
2017-06-03 20:48:00 +0530

    added js for dropdown
    Check and confirm

--- js/script.js
+++ js/script.js
@@ -5,4 +5,13 @@ $(document).ready(function()
 	var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 	$('#date').html("Date: " + today.toLocaleDateString('en-US', options));
 	//date
+
+	//hover dropdown
+	$('ul.nav li.dropdown').hover(function() {
+  		$(this).find('.dropdown-menu').stop(true, true).delay(100).slideToggle(300);
+	}, 
+	function() {
+  		$(this).find('.dropdown-menu').stop(true, true).delay(100).slideToggle(300);
+	});
+	//hover dropdown
 });
