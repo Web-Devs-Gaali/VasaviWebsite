5cefa7f043416b0b40c4af6c49a4493d41ab2e4f
krushiraj <krushiraj123@gmail.com>
2017-06-08 23:23:36 +0530

    fixed conflicts

--- login.php
+++ login.php
@@ -1,23 +1,38 @@
 <?php
-	require("login.html");
+	$username = "1602-15-737-073"; 
+$password = "chlore"; 
+$fields = "txtLoginID=" . $username . "&txtPWD=" . $password . "&btnLogin=true"; 
+$ch=curl_init(); 
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1); 
 
-	$postData = array(
-    'txtLoginID' => '1602-15-737-073',
-    'txtPWD' => 'chlore',
-    'btnLogin' => true,
-    'redirect_to' => 'http://vce.ac.in/index.aspx',
-    'testcookie' => '1'
-	);
+curl_setopt($ch, CURLOPT_URL, 'http://vce.ac.in/index.aspx'); 
+curl_setopt($ch, CURLOPT_HEADER, 0); 
+curl_setopt($ch, CURLOPT_COOKIEJAR, "ASPSESSIONIDSQADDRBQ=OCALKIGABGLPCJLKMGIKJLLF"); //I got this exact cookiename from LiveHTTPheaders (Firefox extension)
+curl_setopt($ch, CURLOPT_COOKIEFILE, "ASPSESSIONIDSQADDRBQ=OCALKIGABGLPCJLKMGIKJLLF"); 
+curl_setopt($ch, CURLOPT_COOKIESESSION, TRUE); 
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 1000 ); 
+curl_setopt($ch, CURLOPT_TIMEOUT, 10000 ); 
+curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.0.8) Gecko/20061025 Firefox/1.5.0.8' ); 
+curl_setopt($ch, CURLOPT_AUTOREFERER, 1 ); 
+curl_setopt ($ch, CURLOPT_REFERER, "http://vce.ac.in"); 
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0 ); 
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0 ); 
+curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1); 
+curl_setopt($ch, CURLOPT_POST, 1); 
+curl_setopt($ch, CURLOPT_POSTFIELDS, $fields); 
+ob_start(); //the issue with the script remains with/without ob_start/ob_end_clean
+$raw_data=curl_exec($ch); 
+curl_setopt($ch, CURLOPT_POST, 0); //at this point im trying to get another page using curl. since i will not be using post i have set it to 0.
+curl_setopt($ch, CURLOPT_URL, 'http://vce.ac.in'); 
+$ttable = curl_exec($ch); 
+ob_end_clean(); 
+echo $ttable; 
+//curl_close($ch); 
+//$raw_data = preg_replace('/\s(1,)/',' ',$raw_data); //clean it up 
+//echo $raw_data; 
+//$raw_data = stripslashes($raw_data); 
+//$raw_data = explode("<strong>",$raw_data); 
+//2 3 and 4 
+//echo htmlentities($raw_data[4]); 
+?> 
 
-	$ch=curl_init();
-	curl_setopt_array($ch, array(
-    CURLOPT_URL => 'http:/vce.ac.in',
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_POST => true,
-    CURLOPT_POSTFIELDS => $postData,
-    CURLOPT_FOLLOWLOCATION => true
-	));
-
-$output = curl_exec($ch);
-echo $output;
-?>
